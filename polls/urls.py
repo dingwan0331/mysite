62c981bc079a5eaa7a050e82242140ad921a4370
dingwan <dingwan0331@gmail.com>
2022-06-01 19:34:41 +0900

    django myapp first_done

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail , name= 'detail'),
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),    detail 뷰의 url을 polls/specifics/~~/ 로 나오게 하고싶을때
     path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:quesion_id>/vote', views.vote, name='vote')
+    path('<int:question_id>/vote', views.vote, name='vote')
 ]
